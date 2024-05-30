8111bd47249f0dd4b78d2a9abbfe0561fe57ecbb
GlebStolupin <109235282+GlebStolupin@users.noreply.github.com>
2024-05-30 22:04:37 +0300

    321

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ["*","http://localhost:3000", "http://localhost:3001", "https://backend-8-medvedev.nomoredomainswork.ru", "http://backend-8-medvedev.nomoredomainswork.ru", "https://frontend-8-medvedev.nomoredomainswork.ru", "http://frontend-8-medvedev.nomoredomainswork.ru"];
+const allowedCors = ["*","http://localhost:3000", "http://localhost:3001", "https://mymainproject.nomorepartiesco.ru", "http://mymainproject.nomorepartiesco.ru", "https://frontend-8-medvedev.nomoredomainswork.ru", "http://frontend-8-medvedev.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
